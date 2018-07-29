1fc8e8c8f1437e68214d2cd741f025b006627eaa
xaenimax <xaenimax.noreply@github.com>
2018-07-29 15:46:01 +0200

    Google fit recording session

---
+++ app/src/main/java/com/udacity/xaenimax/runmyway/ui/completedsession/CompletedSessionActivity.java
@@ -0,0 +1,13 @@
+package com.udacity.xaenimax.runmyway.ui.completedsession;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CompletedSessionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completed_session);
+    }
+}
